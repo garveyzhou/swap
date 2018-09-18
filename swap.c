c2b1e7617f26087340ee62831a168342fb65e224
GarveyZhou <gzhou2@stuy.edu>
2018-09-18 16:40:54 -0400

    finish hw

---
+++ swap.c
@@ -0,0 +1,27 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <time.h>
+
+int main(){
+  int ary[10];
+  srand( time(NULL) );
+  int i = 0;
+  while(i < 10){
+    ary[i] = rand();
+    i++;
+  }
+  ary[9] = 0;
+  i = 0;
+  while(i < 10){
+    printf("ary[%d] = %d\n",i,ary[i]);
+    i++;
+  }
+  int ray[10];
+  i = 9;
+  while(i >= 0){
+    ray[i] = *(ary +i);
+    printf("ray[%d] = %d\n",i,ray[i]);
+    i--;
+  }
+  return 0;
+}
