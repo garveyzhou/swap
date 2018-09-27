89aa0c6647b0d21718fc62636141edb606f5d309
unknown <gzhou2@stuy.edu>
2018-09-26 21:57:35 -0400

    added pointers

--- demo.c
+++ demo.c
@@ -4,13 +4,13 @@
 int main(){
   char source[10] = "Hello";
   char destin[10] = "NICE ONE";
-  printf("String 1 = %s\n",source);
-  printf("String 2 = %s\n",destin);
+  
+  printf("String 1 = %s  Hex: %x\n",source,&source);
+  printf("String 2 = %s  Hex: %x\n",destin,&destin);
 
   strcpy(destin,source);
-
-  printf("String 1 = %s\n",source);
-  printf("String 2 = %s\n",destin);
+  printf("String 1 = %s  Hex: %x\n",source,&source);
+  printf("String 2 = %s  Hex: %x\n",destin,&destin);
 
   return 0;
 }
